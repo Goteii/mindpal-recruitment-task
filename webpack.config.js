2cfc2a0e7c416483d4e042dce1cd4ce3207e6a98
Goteii <deyssog@gmail.com>
2024-08-31 13:38:39 +0200

    project init

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const ENTRY_PATH = path.resolve(__dirname, "src/index");
+const DIST_PATH = path.resolve(__dirname, "dist");
+const TEMPLATE_PATH = path.resolve(__dirname, "src/index.html");
+
+module.exports = {
+  entry: {
+    main: ENTRY_PATH,
+  },
+  output: {
+    path: DIST_PATH,
+    filename: "[name].[contenthash].js",
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: TEMPLATE_PATH,
+    }),
+  ],
+  devtool: "inline-source-map",
+  devServer: {
+    static: DIST_PATH,
+    hot: true,
+  },
+};
